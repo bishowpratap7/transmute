3d63a5a4df89178f896a9cd72bebcf053c60e2ce
bishowpratap7 <bishowpratappandey@gmail.com>
2023-01-08 12:07:50 -0500

    Added Maven and Java Files for Initial Setups

---
+++ src/test/java/com/transmute/conversion/ConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transmute.conversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
