621306a93e0e00d0d35cb26a2bb4dd5dec255c1e
bishowpratap7 <bishowpratappandey@gmail.com>
2023-03-10 16:51:20 -0500

    WIP

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Transmute
 
-This is an open source project designed to help community with 100% free conversions of word docx containing foreign languages to 
+This is an open source project designed to help community with 100% free conversions of word docx containing foreign
+languages to
 PDF for FREE. No need to install fonts on your machine and at the same time preserve the formatting.
 This project is suitable for both personal and enterprise use.
 
 With Transmute we can:
-1. Convert Word Docx containing foreign languages (non english) characters to PDF without losing the language formatting.
+
+1. Convert Word Docx containing foreign languages (non english) characters to PDF without losing the language
+   formatting.
 
 Current Challenges:
+
 1. Can't handle Macros and scenarios involving content control boxes.
 
 Asking for Help:
--- src/main/java/com/transmute/docx/to/pdf/api/DocxToPDFController.java
+++ src/main/java/com/transmute/docx/to/pdf/api/DocxToPDFController.java
@@ -3,10 +3,10 @@ package com.transmute.docx.to.pdf.api;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.transmute.docx.to.pdf.DocxToPDF;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.docx4j.Docx4J;
 import org.docx4j.convert.out.HTMLSettings;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -26,49 +26,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 @RestController
 public class DocxToPDFController {
 
+    private static final String CHROME_DRIVER_PATH = "src/main/resources/static/chromedriver_windows/chromedriver.exe";
+    private static final ChromeOptions CHROME_OPTIONS = chromeOptions();
+    private static final String TEMP_HTML_PATH = "C:\\ui_be_projects\\transmute\\temp\\test.html";
+    private static final String TEMP_PNG_PATH = "C:\\ui_be_projects\\transmute\\temp\\test.png";
+    private static final String PDF_FILE_PATH = "C:\\ui_be_projects\\transmute\\converted_docx_to_pdf\\test.pdf";
+
+    private static final String HTML_SETTINGS_IMG = "C:\\ui_be_projects\\transmute\\temp\\";
     private final DocxToPDF docxToPDF;
 
     public DocxToPDFController(DocxToPDF docxToPDF) {
         this.docxToPDF = docxToPDF;
     }
 
-    @PostMapping("/convertToPDF")
-    public ResponseEntity<byte[]> responseEntity(@RequestParam("file") MultipartFile[] files) throws IOException, DocumentException, ParserConfigurationException {
-        byte[] fileContent = Arrays.stream(files).findFirst().get().getBytes();
-
-        InputStream is = new BufferedInputStream(new ByteArrayInputStream(fileContent));
-
-
-        byte[] pdfDocBytes = new byte[0];
-
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-            WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage
-                    .load(is);
-            HTMLSettings htmlSettings = Docx4J.createHTMLSettings();
-            htmlSettings.setImageDirPath("C:\\ui_be_projects\\transmute\\converted_documents\\");
-            htmlSettings.setWmlPackage(wordMLPackage);
-
-            Docx4J.toHTML(htmlSettings, out, Docx4J.FLAG_EXPORT_PREFER_XSL);
-
-            Path path = Paths.get("C:\\ui_be_projects\\transmute\\test.html");
-            Files.write(path, out.toByteArray());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        WebDriverManager.chromedriver().setup();
+    private static ChromeOptions chromeOptions() {
 
         //set the location of chrome browser
-        System.setProperty("webdriver.chrome.driver", "C:\\ui_be_projects\\transmute\\src\\main\\resources\\static\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
         System.setProperty("webdriver.chrome.whitelistedIps", "");
         System.setProperty("--allowed-ips", "");
-
 
         // Setting your Chrome options (Desired capabilities)
         ChromeOptions options = new ChromeOptions();
@@ -85,23 +65,54 @@ public class DocxToPDFController {
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--remote-debugging-port=9222");
 
-        ChromeDriverService.createServiceWithConfig(options);
+        return options;
+    }
+
+    @PostMapping("/convertToPDF")
+    public ResponseEntity<byte[]> responseEntity(@RequestParam("file") MultipartFile[] files, boolean saveAsFile, boolean saveAsByteArray) throws IOException, DocumentException, ParserConfigurationException {
+        byte[] fileContent = Arrays.stream(files).findFirst().get().getBytes();
+
+        InputStream is = new BufferedInputStream(new ByteArrayInputStream(fileContent));
+
+
+        byte[] pdfDocBytes = new byte[0];
+
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage
+                    .load(is);
+            HTMLSettings htmlSettings = Docx4J.createHTMLSettings();
+            htmlSettings.setImageDirPath(HTML_SETTINGS_IMG);
+            htmlSettings.setWmlPackage(wordMLPackage);
+
+            Docx4J.toHTML(htmlSettings, out, Docx4J.FLAG_EXPORT_PREFER_XSL);
+
+            Path path = Paths.get(TEMP_HTML_PATH);
+            Files.write(path, out.toByteArray());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        WebDriverManager.chromedriver().setup();
+
+
+        ChromeDriverService.createServiceWithConfig(CHROME_OPTIONS);
 
         // Initialize browser
-        ChromeDriver driver = new ChromeDriver(options);
+        ChromeDriver driver = new ChromeDriver(CHROME_OPTIONS);
 
         //navigate to url
-        driver.get("C:\\ui_be_projects\\transmute\\test.html");
+        driver.get(TEMP_HTML_PATH);
 
         // capture screenshot and store the image
         Screenshot s = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
-        ImageIO.write(s.getImage(), "PNG", new File("C:\\ui_be_projects\\transmute\\test.png"));
+        ImageIO.write(s.getImage(), "PNG", new File(TEMP_PNG_PATH));
 
         //closing the webdriver
         driver.close();
 
 
-        Image image = Image.getInstance("C:\\ui_be_projects\\transmute\\test.png");
+        Image image = Image.getInstance(TEMP_PNG_PATH);
         image.scaleToFit(PageSize.A0);
         //Get Size of Original Image for conversion
         float origWidth = image.getWidth();
@@ -113,7 +124,7 @@ public class DocxToPDFController {
         Rectangle rectangle = new Rectangle(origWidth, origHeight);
 
         Document document = new Document(PageSize.A4, 10, 10, 10, 10);
-        PdfWriter.getInstance(document, new FileOutputStream("C:\\ui_be_projects\\transmute\\test.pdf"));
+        PdfWriter.getInstance(document, new FileOutputStream(PDF_FILE_PATH));
         document.open();
         //Set page size before adding new page
         document.setPageSize(rectangle);
